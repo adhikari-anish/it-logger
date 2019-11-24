72118d2bef21f2be2e44d760258f8f5352f64ee5
anish <microanish@gmail.com>
2019-11-24 17:27:55 +0545

    Completed UI for it-logger

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -20,7 +20,7 @@ const LogItem = ({ log }) => {
           <span className="black-text">{log.tech}</span> on{" "}
           <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
-        <a href="#" className="secondary-content">
+        <a href="#!" className="secondary-content">
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
