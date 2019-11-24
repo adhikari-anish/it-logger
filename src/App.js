ab87e80066e4d7646be359d3c90fe5a50f25733c
anish <microanish@gmail.com>
2019-11-24 14:33:20 +0545

    Added search-bar system-logs-container add-log-modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
-  return <div className="App">My Aoo</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
