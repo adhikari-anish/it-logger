5dfc7c6cea6f0bb184518a24b1f1ef93055a6d8f
anish <microanish@gmail.com>
2019-11-27 15:27:46 +0545

    completed redux part for logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Moment from "react-moment";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { deleteLog, setCurrent } from "../../actions/logActions";
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   return (
     <li className="collection-item">
       <div>
@@ -11,6 +13,7 @@ const LogItem = ({ log }) => {
           className={`modal-trigger ${
             log.attention ? "red-text" : "blue-text"
           }`}
+          onClick={() => setCurrent(log)}
         >
           {log.message}
         </a>
@@ -20,7 +23,11 @@ const LogItem = ({ log }) => {
           <span className="black-text">{log.tech}</span> on{" "}
           <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
+        <a
+          href="#!"
+          className="secondary-content"
+          onClick={() => deleteLog(log.id)}
+        >
           <i className="material-icons grey-text">delete</i>
         </a>
       </div>
@@ -29,7 +36,9 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(null, { deleteLog, setCurrent })(LogItem);
